24e18eb5a38d658441aed71f6081ef5feba7ae89
TPCAD <1814083420@qq.com>
2024-08-24 10:49:20 +0800

    lib: memory

---
+++ include/memory.h
@@ -0,0 +1,14 @@
+#ifndef EASYSTL_MEMORY_H
+#define EASYSTL_MEMORY_H
+
+#include "uninitialized.h"
+
+namespace easystl {
+
+template <class Tp> constexpr Tp *address_of(Tp &value) noexcept {
+    return &value;
+}
+
+} // namespace easystl
+
+#endif // !EASYSTL_MEMORY_H
